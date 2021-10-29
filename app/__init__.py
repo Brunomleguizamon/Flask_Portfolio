7b3609ec9be75b32eabacb63d8ff6139c68d2575
Bmleguizamon <brunomleguizamon@gmail.com>
2021-10-29 20:54:36 +0200

    flask app init

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY')
+            )
