c432d2e2535b09fd5da50faf2fdddfa7c28a49b2
Bmleguizamon <brunomleguizamon@gmail.com>
2021-11-15 23:10:58 +0100

    requirements created

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,13 @@
 from flask import(
     Blueprint,
-    render_template
+    render_template,
+    request,
+    redirect,
+    url_for,
+    current_app
 )
+from flask.helpers import send_file, url_for
+from sendgrid.helpers.mail import *
 
 bp = Blueprint('portfolio', __name__, url_prefix='/')
 
@@ -10,6 +16,20 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 def index():
     return render_template('portfolio/index.html')
 
-@bp.route('/mail', methods=['POST','GET']) 
+
+@bp.route('/mail', methods=['POST', 'GET'])
 def mail():
-    return render_template('portfolio/send_mail.html')
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    if request == 'POST':
+        send_email(name, email, message)
+        return render_template('portfolio/send_mail.html')
+    else:
+        return redirect(url_for('portfolio.index'))
+
+
+def send_email(name, email, message):
+    mi_email = 'brunomleguizamon@gmail.com'
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
