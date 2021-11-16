4f2912f549ceb130481d084971c7b2f4a56e5671
Bmleguizamon <brunomleguizamon@gmail.com>
2021-11-16 01:39:15 +0100

    urequiremeents update

--- app/portfolio.py
+++ app/portfolio.py
@@ -7,8 +7,8 @@ from flask import(
     current_app
 )
 from flask.helpers import send_file, url_for
+from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import *
-
 bp = Blueprint('portfolio', __name__, url_prefix='/')
 
 
@@ -33,3 +33,21 @@ def mail():
 def send_email(name, email, message):
     mi_email = 'brunomleguizamon@gmail.com'
     sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+
+    from_email = Email(mi_email)
+    to_email = To(mi_email, substitutions={
+        "-name-": name,
+        "-email-": email,
+        "-message-": message,
+    })
+
+    html_content = """
+        <p>Hola Bruno! tienes un nuevo contacto desde la pagina web </p>
+        <p>Nombre: -name- </p>
+        <p>Email: -email- </p>
+        <p>Mensaje: -message-  </p>
+    """
+
+    mail = Mail(mi_email, to_email,
+                'Nuevo contacto desde el portfolio', html_content=html_content)
+    response = sg.client.mail.send.post(request_body=mail.get())
